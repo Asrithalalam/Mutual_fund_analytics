df995eb74795fcb1a17deb36424c7009629b677c
Asritha Lalam <lalamasritha2004@gmail.com>
2026-06-28 15:17:20 +0530

    committed all remaining csv files

--- data_cleaning.py
+++ data_cleaning.py
@@ -11,7 +11,7 @@ print("\nscheme_performance columns:",scheme_performance.columns.tolist())
 print("\ninvestor_transactions columns:",investor_transactions.columns.tolist())   
 
 # Data Cleaning Steps for each file
-
+#Dataset: 02_nav_history.csv
 def clean_nav_data(nav_data):
     
     # Convert 'date' column to datetime format
@@ -36,6 +36,7 @@ def clean_nav_data(nav_data):
     
     return nav_data
 
+#Dataset: 08_investor_transactions.csv
 def clean_investor_transactions(investor_transactions):
     
     #date format
@@ -73,6 +74,7 @@ def clean_investor_transactions(investor_transactions):
     
     return investor_transactions
 
+#Dataset: 07_scheme_performance.csv
 def clean_scheme_performance(scheme_performance):
      
     #validate all return values are numeric
@@ -94,8 +96,339 @@ def clean_scheme_performance(scheme_performance):
     
     return scheme_performance
 
+#cleaning remaining datasets
+fund_master = pd.read_csv("data/raw/01_fund_master.csv")
+aum_data = pd.read_csv("data/raw/03_aum_by_fund_house.csv")
+sip_inflows = pd.read_csv("data/raw/04_monthly_sip_inflows.csv")
+category_inflows = pd.read_csv("data/raw/05_category_inflows.csv")
+folio_count = pd.read_csv("data/raw/06_industry_folio_count.csv")
+portfolio_holdings = pd.read_csv("data/raw/09_portfolio_holdings.csv")
+benchmark_indices = pd.read_csv("data/raw/10_benchmark_indices.csv")
+
+#Defining cleaning functions for the remaining datasets
+#Dataset: 01_fund_master.csv
+def clean_fund_master(fund_master):
+    # Remove duplicate rows
+    fund_master = fund_master.drop_duplicates()
+
+    # Remove leading/trailing spaces from text columns
+    text_columns = ['fund_house', 'scheme_name', 'category',
+                    'sub_category', 'plan', 'benchmark',
+                    'fund_manager', 'risk_category',
+                    'sebi_category_code']
+
+    for column in text_columns:
+        fund_master[column] = fund_master[column].str.strip()
+
+    # Convert launch_date to datetime
+    fund_master['launch_date'] = pd.to_datetime(
+        fund_master['launch_date'],
+        errors='coerce'
+    )
+
+    # Remove rows with missing AMFI code
+    fund_master = fund_master.dropna(subset=['amfi_code'])
+
+    # Remove duplicate AMFI codes
+    fund_master = fund_master.drop_duplicates(subset='amfi_code')
+
+    # Expense ratio validation
+    fund_master = fund_master[
+        (fund_master['expense_ratio_pct'] >= 0) &
+        (fund_master['expense_ratio_pct'] <= 5)
+    ]
+
+    # Exit load validation
+    fund_master = fund_master[
+        fund_master['exit_load_pct'] >= 0
+    ]
+
+    fund_master.to_csv(
+        "data/processed/01_fund_master_cleaned.csv",
+        index=False
+    )
+
+    print("Fund Master cleaned successfully.")
+
+    return fund_master
+
+#Dataset: 03_aum_by_fund_house.csv
+def clean_aum_by_fund_house(aum_data):
+
+    # Remove duplicates
+    aum_data = aum_data.drop_duplicates()
+
+    # Date conversion
+    aum_data['date'] = pd.to_datetime(
+        aum_data['date'],
+        errors='coerce'
+    )
+
+    # Remove spaces
+    aum_data['fund_house'] = aum_data['fund_house'].str.strip()
+
+    # Numeric conversion
+    numeric_cols = [
+        'aum_lakh_crore',
+        'aum_crore',
+        'num_schemes'
+    ]
+
+    for column in numeric_cols:
+        aum_data[column] = pd.to_numeric(aum_data[column], errors='coerce')
+
+    # Keep positive values only
+    aum_data = aum_data[
+        (aum_data['aum_crore'] > 0) &
+        (aum_data['aum_lakh_crore'] > 0) &
+        (aum_data['num_schemes'] > 0)
+    ]
+
+    aum_data.to_csv(
+        "data/processed/03_aum_by_fund_house_cleaned.csv",
+        index=False
+    )
+
+    print("AUM dataset cleaned successfully.")
+
+    return aum_data
+
+#Dataset: 04_monthly_sip_inflows.csv
+def clean_monthly_sip_inflows(sip_inflows):
+
+    # Remove duplicates
+    sip_inflows = sip_inflows.drop_duplicates()
+
+    # Convert month
+    sip_inflows['month'] = pd.to_datetime(
+        sip_inflows['month'],
+        format='%Y-%m',
+        errors='coerce'
+    )
+
+    # Convert numeric columns
+    numeric_cols = [
+        'sip_inflow_crore',
+        'active_sip_accounts_crore',
+        'new_sip_accounts_lakh',
+        'sip_aum_lakh_crore',
+        'yoy_growth_pct'
+    ]
+
+    for column in numeric_cols:
+        sip_inflows[column] = pd.to_numeric(
+            sip_inflows[column],
+            errors='coerce'
+        )
+
+    # Validate positive values
+    sip_inflows = sip_inflows[
+        (sip_inflows['sip_inflow_crore'] >= 0) &
+        (sip_inflows['active_sip_accounts_crore'] >= 0) &
+        (sip_inflows['new_sip_accounts_lakh'] >= 0) &
+        (sip_inflows['sip_aum_lakh_crore'] >= 0)
+    ]
+
+    sip_inflows = sip_inflows.sort_values('month')
+
+    sip_inflows.to_csv(
+        "data/processed/04_monthly_sip_inflows_cleaned.csv",
+        index=False
+    )
+
+    print("Monthly SIP Inflows cleaned successfully.")
+
+    return sip_inflows
+
+#Dataset: 05_category_inflows.csv
+def clean_category_inflows(category_inflows):
+
+    # Remove duplicate rows
+    category_inflows = category_inflows.drop_duplicates()
+
+    # Convert month to datetime
+    category_inflows['month'] = pd.to_datetime(
+        category_inflows['month'],
+        format='%Y-%m',
+        errors='coerce'
+    )
+
+    # Remove leading/trailing spaces
+    category_inflows['category'] = category_inflows['category'].str.strip()
+
+    # Convert inflow to numeric
+    category_inflows['net_inflow_crore'] = pd.to_numeric(
+        category_inflows['net_inflow_crore'],
+        errors='coerce'
+    )
+
+    # Keep valid inflows
+    category_inflows = category_inflows[
+        category_inflows['net_inflow_crore'].notnull()
+    ]
+
+    # Save cleaned dataset
+    category_inflows.to_csv(
+        "data/processed/05_category_inflows_cleaned.csv",
+        index=False
+    )
+
+    print("Category Inflows cleaned successfully.")
+
+    return category_inflows
+
+#Dataset: 06_industry_folio_count.csv
+def clean_industry_folio_count(folio_count):
+
+    # Remove duplicate rows
+    folio_count = folio_count.drop_duplicates()
+
+    # Convert month
+    folio_count['month'] = pd.to_datetime(
+        folio_count['month'],
+        format='%Y-%m',
+        errors='coerce'
+    )
+
+    # Convert numeric columns
+    numeric_cols = [
+        'total_folios_crore',
+        'equity_folios_crore',
+        'debt_folios_crore',
+        'hybrid_folios_crore',
+        'others_folios_crore'
+    ]
+
+    for column in numeric_cols:
+        folio_count[column] = pd.to_numeric(
+            folio_count[column],
+            errors='coerce'
+        )
+
+    # Keep positive values
+    for column in numeric_cols:
+        folio_count = folio_count[
+            folio_count[column] >= 0
+        ]
+
+    # Save cleaned dataset
+    folio_count.to_csv(
+        "data/processed/06_industry_folio_count_cleaned.csv",
+        index=False
+    )
+
+    print("Industry Folio Count cleaned successfully.")
+
+    return folio_count
+
+#Dataset: 09_portfolio_holdings.csv
+def clean_portfolio_holdings(portfolio_holdings):
+
+    # Remove duplicate rows
+    portfolio_holdings = portfolio_holdings.drop_duplicates()
+
+    # Convert portfolio date
+    portfolio_holdings['portfolio_date'] = pd.to_datetime(
+        portfolio_holdings['portfolio_date'],
+        errors='coerce'
+    )
+
+    # Remove spaces
+    text_cols = [
+        'stock_symbol',
+        'stock_name',
+        'sector'
+    ]
+
+    for column in text_cols:
+        portfolio_holdings[column] = portfolio_holdings[column].str.strip()
+
+    # Convert numeric columns
+    numeric_cols = [
+        'weight_pct',
+        'market_value_cr',
+        'current_price_inr'
+    ]
+
+    for column in numeric_cols:
+        portfolio_holdings[column] = pd.to_numeric(
+            portfolio_holdings[column],
+            errors='coerce'
+        )
+
+    # Weight validation
+    portfolio_holdings = portfolio_holdings[
+        (portfolio_holdings['weight_pct'] >= 0) &
+        (portfolio_holdings['weight_pct'] <= 100)
+    ]
+
+    # Positive values
+    portfolio_holdings = portfolio_holdings[
+        (portfolio_holdings['market_value_cr'] >= 0) &
+        (portfolio_holdings['current_price_inr'] > 0)
+    ]
+
+    # Sort
+    portfolio_holdings = portfolio_holdings.sort_values(
+        ['amfi_code', 'portfolio_date']
+    )
+
+    # Save cleaned dataset
+    portfolio_holdings.to_csv(
+        "data/processed/09_portfolio_holdings_cleaned.csv",
+        index=False
+    )
+
+    print("Portfolio Holdings cleaned successfully.")
+
+    return portfolio_holdings
+
+#Dataset: 10_benchmark_indices.csv
+def clean_benchmark_indices(benchmark_indices):
+
+    # Remove duplicates
+    benchmark_indices = benchmark_indices.drop_duplicates()
+
+    # Convert date
+    benchmark_indices['date'] = pd.to_datetime(
+        benchmark_indices['date'],
+        errors='coerce'
+    )
+
+    # Remove spaces
+    benchmark_indices['index_name'] = benchmark_indices['index_name'].str.strip()
+
+    # Convert close value
+    benchmark_indices['close_value'] = pd.to_numeric(
+        benchmark_indices['close_value'],
+        errors='coerce'
+    )
+
+    # Positive values only
+    benchmark_indices = benchmark_indices[
+        benchmark_indices['close_value'] > 0
+    ]
+
+    benchmark_indices.to_csv(
+        "data/processed/10_benchmark_indices_cleaned.csv",
+        index=False
+    )
+
+    print("Benchmark Indices cleaned successfully.")
+
+    return benchmark_indices
+
+
 if __name__ == "__main__":
     # Clean each dataset
     cleaned_nav_data = clean_nav_data(nav_data)
     cleaned_investor_transactions = clean_investor_transactions(investor_transactions)
     cleaned_scheme_performance = clean_scheme_performance(scheme_performance)
+    cleaned_fund_master = clean_fund_master(fund_master)
+    cleaned_aum_by_fund_house = clean_aum_by_fund_house(aum_data)
+    cleaned_monthly_sip_inflows = clean_monthly_sip_inflows(sip_inflows)
+    cleaned_category_inflows = clean_category_inflows(category_inflows)
+    cleaned_industry_folio_count = clean_industry_folio_count(folio_count)
+    cleaned_portfolio_holdings = clean_portfolio_holdings(portfolio_holdings)
+    cleaned_benchmark_indices = clean_benchmark_indices(benchmark_indices)
+    
